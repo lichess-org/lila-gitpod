317a3579ee39c94df70440d0640c480700aae48b
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-01-14 23:00:56 -0500

    Add/document other optional services (#22)

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,4 +3,4 @@
 # Serve the lila-gitpod docs
 
 python3.9 -m pip install mkdocs mkdocs-material
-python3.9 -m mkdocs serve --config-file /workspace/lila-gitpod/mkdocs.yml 
+python3.9 -m mkdocs serve --config-file /workspace/lila-gitpod/mkdocs.yml
